b46f9e2d59b894daec67c058b4d8f2eb6ee37a19
Alena Hrynkevich <alena@appveyor.com>
2019-08-14 11:52:09 -0700

    Added $LastExitCode check

--- after_build.ps1
+++ after_build.ps1
@@ -1,8 +1,10 @@
 $env:TEST_CONNECTION_STRING = 'Server=(local)\SQL2014;Database=MyDatabase_test;User ID=sa;Password=Password12!'
 cmd /c where sqlpackage
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 # deploy .dacpac package to local SQL Server instance for integration testing
 #  - '"C:\Program Files\IIS\Microsoft Web Deploy V3\msdeploy.exe" -verb:sync -source:dbDacFx="%appveyor_build_folder%\MyDatabase\bin\Debug\MyDatabase.dacpac" -dest:dbDacFx="%TEST_CONNECTION_STRING%",dacpacAction=Deploy,CreateNewDatabase=True'
 sqlpackage /Action:Publish /SourceFile:"$env:appveyor_build_folder\MyDatabase\bin\Debug\MyDatabase.dacpac" /TargetConnectionString:"$env:TEST_CONNECTION_STRING"
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
 $conn = New-Object System.Data.SqlClient.SqlConnection
 $conn.ConnectionString = $env:TEST_CONNECTION_STRING
 $conn.Open()
@@ -13,3 +15,4 @@ while($reader.Read())
   Write-Host "Table: $($reader["table_name"])"
 }
 $conn.Close()
+if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
